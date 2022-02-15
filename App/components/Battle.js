c926376eebc3b247212a4fd893818d20d4b9a53d
Lina Zoo <linazooper@gmail.com>
2022-02-15 12:11:39 -0600

    Build out the component to get the two github user names. Created a new component and abstracted all the logic into that new component. added css for my new playerinput component

--- App/components/Battle.js
+++ App/components/Battle.js
@@ -1,5 +1,6 @@
-import React from "react";
+import * as React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
   return (
@@ -10,28 +11,87 @@ function Instructions() {
           <h3 className="header-sm">Enter two Github users</h3>
           <FaUserFriends
             className="bg-light"
-            color="rgb(255, 191, 116"
+            color="rgb(255, 191, 116)"
             size={140}
           />
         </li>
         <li>
           <h3 className="header-sm">Battle</h3>
-          <FaFighterJet className="bg-light" color="727272" size={140} />
+          <FaFighterJet className="bg-light" color="#727272" size={140} />
         </li>
         <li>
-          <h3 className="header-sm">See the Winners!</h3>
-          <FaTrophy className="bg-light" color="rgb(255,215,0" size={140} />
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
         </li>
       </ol>
     </div>
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+  handleChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <form className="column player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            className="input-light"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className="btn dark-btn"
+            type="submit"
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default class Battle extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+        <PlayerInput
+          label="Label!"
+          onSubmit={(value) => console.log("value!", value)}
+        />
       </React.Fragment>
     );
   }
