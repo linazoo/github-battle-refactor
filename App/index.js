31d76e2ea6a78cff8ee5ffda24402f7061a7d400
Lina Zoo <linazooper@gmail.com>
2022-02-24 10:27:48 -0600

    created a context and wrapped my app in themeprovider passing the value of state so I can use the consumer anywhere to toggle the theme

--- App/index.js
+++ App/index.js
@@ -2,15 +2,29 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import Battle from "./components/Battle";
-
 import Popular from "./components/Popular";
+import { ThemeProvider } from "./contexts/theme";
 
 class App extends React.Component {
+  constructor() {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Popular />
+        </div>
+      </ThemeProvider>
     );
   }
 }
