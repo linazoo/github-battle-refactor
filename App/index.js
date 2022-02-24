84ed76b30a01130ad1ef2439ac051ff4db07aeef
Lina Zoo <linazooper@gmail.com>
2022-02-24 15:39:00 -0600

    imported Theme Consumer from the contexts to change the styles in any of the components in the app

--- App/index.js
+++ App/index.js
@@ -25,7 +25,7 @@ class App extends React.Component {
         <div className={this.state.theme}>
           <div className="container">
             <Nav />
-            <Popular />
+            <Battle />
           </div>
         </div>
       </ThemeProvider>
