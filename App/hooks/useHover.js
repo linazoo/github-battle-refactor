2e4b6c1a190ede848d3bc94e3b50080a14f331cb
Lina Zoo <linazooper@gmail.com>
2022-10-11 15:34:17 -0500

    refactoring, debugging ToolTip component. using a custom hook instead of another component to make the onHover states

---
+++ App/hooks/useHover.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const useHover = () => {
+  const [hovering, setHovering] = useState(false);
+
+  const onMouseOver = () => setHovering(true);
+  const onMouseOut = () => setHovering(false);
+
+  return [
+    hovering,
+    {
+      onMouseOut,
+      onMouseOver,
+    },
+  ];
+};
+
+export default useHover;
