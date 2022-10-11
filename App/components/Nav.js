35d877a2b8667ce9e087b45d0ef6fba21f544ab4
Lina Zoo <linazooper@gmail.com>
2022-10-11 14:35:28 -0500

    first commit, refactoring to use functional components

--- App/components/Nav.js
+++ App/components/Nav.js
@@ -5,10 +5,10 @@ import { NavLink } from "react-router-dom";
 const activeStyle = {
   color: "rgb(187,46,31)",
 };
-export default function Nav() {
+export default function Nav({ toggleTheme }) {
   return (
     <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
+      {(theme) => (
         <nav className="row space-between">
           <ul className="row nav">
             <li>
